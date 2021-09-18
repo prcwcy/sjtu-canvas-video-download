c13c026ff2ed5b3cbbfb3608c37aea9998cc7e4d
prcwcy <prcwcy@prcwcy>
2021-09-18 16:46:50 +0800

    remember username and password

--- sjtu_login_frame.py
+++ sjtu_login_frame.py
@@ -1,8 +1,15 @@
 import tkinter as tk
+import tkinter.ttk
 import tkinter.messagebox
+import sys
+import os
+import json
 from bs4 import BeautifulSoup
 from PIL import ImageTk
 from sjtu_login import *
+
+self_dirname = os.path.dirname(sys.argv[0])
+config_filename = os.path.join(self_dirname, "config.json")
 
 
 class LoginFrame(tk.Frame):
@@ -12,26 +19,51 @@ class LoginFrame(tk.Frame):
 
         entry_columnspan = 8
 
-        for i in range(entry_columnspan+1):
+        for i in range(1+entry_columnspan+1):
             self.columnconfigure(i, weight=1)
 
         num_row = 0
+
+        if os.path.isfile(config_filename):
+            with open(config_filename, encoding="utf-8") as config_file:
+                config = json.load(config_file)
+            self.config_username = config["username"]
+            self.config_password = config["password"]
+        else:
+            self.config_username = ""
+            self.config_password = ""
 
         self.url = url
         self.callback = callback
 
         self.username_label = tk.Label(self, text="jAccount用户名")
         self.username_label.grid(column=0, row=num_row)
-        self.username_var = tk.StringVar()
+        self.username_var = tk.StringVar(
+            value=self.config_username
+        )
         self.username_entry = tk.Entry(self, textvariable=self.username_var)
         self.username_entry.grid(
             column=1, columnspan=entry_columnspan, row=num_row, sticky=tk.W+tk.E
+        )
+        self.username_checkbutton_var = tk.IntVar(
+            value=True
+        )
+        self.username_checkbutton = tkinter.ttk.Checkbutton(
+            self,
+            command=self.username_checkbutton_changed,
+            variable=self.username_checkbutton_var,
+            text="记住"
+        )
+        self.username_checkbutton.grid(
+            column=1+entry_columnspan, row=num_row
         )
         num_row += 1
 
         self.password_label = tk.Label(self, text="jAccount密码")
         self.password_label.grid(column=0, row=num_row)
-        self.password_var = tk.StringVar()
+        self.password_var = tk.StringVar(
+            value=self.config_password
+        )
         self.password_entry = tk.Entry(
             self,
             show='*',
@@ -39,6 +71,18 @@ class LoginFrame(tk.Frame):
         )
         self.password_entry.grid(
             column=1, columnspan=entry_columnspan, row=num_row, sticky=tk.W+tk.E
+        )
+        self.password_checkbutton_var = tk.IntVar(
+            value=int(bool(self.config_password))
+        )
+        self.password_checkbutton = tkinter.ttk.Checkbutton(
+            self,
+            command=self.password_checkbutton_changed,
+            variable=self.password_checkbutton_var,
+            text="记住"
+        )
+        self.password_checkbutton.grid(
+            column=1+entry_columnspan, row=num_row
         )
         num_row += 1
 
@@ -132,5 +176,28 @@ class LoginFrame(tk.Frame):
         else:
             tkinter.messagebox.showinfo("登录成功", "登录成功")
             self.master.destroy()
-            if self.callback is not None:
-                self.callback(result.cookies.get_dict())
+            if not self.username_checkbutton_var.get():
+                username = ""
+            if not self.password_checkbutton_var.get():
+                password = ""
+            if self.config_username != username or self.config_password != password:
+                config = {
+                    "username": username,
+                    "password": password
+                }
+                with open(config_filename, "w", encoding="utf-8") as config_file:
+                    json.dump(
+                        config,
+                        config_file,
+                        ensure_ascii=False,
+                        check_circular=False
+                    )
+            self.callback(result.cookies.get_dict())
+
+    def username_checkbutton_changed(self):
+        if not self.username_checkbutton_var.get():
+            self.password_checkbutton_var.set(False)
+
+    def password_checkbutton_changed(self):
+        if self.password_checkbutton_var.get():
+            self.username_checkbutton_var.set(True)
